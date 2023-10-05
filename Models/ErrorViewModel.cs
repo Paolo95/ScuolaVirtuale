e2f95dd086ec5768fc89d70e66e591d82a0eddb4
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-10-05 20:07:24 +0200

    Repo init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ScuolaVirtuale.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
